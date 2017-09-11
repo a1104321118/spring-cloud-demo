642135bd439f8d81823cf857d4a8ea5609db7d29
huangrui <1104321118@qq.com>
2017-09-11 16:50:12 +0800

    新增 zipkin

---
+++ spring-cloud-zipkin-server/src/main/java/com/hr/SpringCloudZipkinServerApplication.java
@@ -0,0 +1,14 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class SpringCloudZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipkinServerApplication.class, args);
+	}
+}
